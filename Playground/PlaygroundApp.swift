a086adf74f201faf479d2e2ccc068297330503d6
gtayl204-ford <gtayl204@ford.com>
2022-10-23 02:20:27 -0400

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Taylor, George (G.E.) on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
